89109d9d3074d2f6066decf1217fc177a3297368
Samuel Getachew <falconroman36@gmail.com>
2024-02-11 12:53:40 +0300

    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: { fancy: ["FancyFont", "cursive"] },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [],
 };
